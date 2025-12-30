7e82b22eab0c702830be940590a88fa02c96a39c
Takeshi Nomura <nomuratakeshi24@gmail.com>
2025-12-30 09:36:40 +0900

    Fix: Renove seaborn from views.py

--- predict/views.py
+++ predict/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 import numpy as np
 # import pandas as pd
 # import matplotlib.pyplot as plt
-import seaborn as sns
+# import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
