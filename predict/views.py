412a42f9cf795ec1e3e6ebab7dd4780fd48f8442
Takeshi Nomura <nomuratakeshi24@gmail.com>
2025-12-30 09:43:09 +0900

    Fix: pandas in views.pys

--- predict/views.py
+++ predict/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 import numpy as np
-# import pandas as pd
+import pandas as pd
 # import matplotlib.pyplot as plt
 # import seaborn as sns
 from sklearn.model_selection import train_test_split
